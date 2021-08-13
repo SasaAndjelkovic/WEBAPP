940cd52c38cd9ff8507f1323f7fb9d93e7967141
SasaAndjelkovic <sandjelkovickg@gmail.com>
2021-08-13 17:41:52 +0200

    new commit

--- react-projects/test-react-project/src/components/App.jsx
+++ react-projects/test-react-project/src/components/App.jsx
@@ -3,7 +3,6 @@ import { Fragment } from 'react';
 import { Header } from './Header/Header.jsx';
 import { BlogList } from './BlogList/BlogList.jsx';
 
-import './App.css';
 
 const App = () => {
   return (
--- react-projects/test-react-project/src/components/BlogList/BlogList.jsx
+++ react-projects/test-react-project/src/components/BlogList/BlogList.jsx
@@ -6,7 +6,7 @@ import './BlogList.css';
 
 export const BlogList = () => (
     <div className = 'blogList'>
-    {blogPosts.map((post, index) => (
+    {blogPosts.map((post, index) => (                 
         <div className = 'blogItem' key = {index}>
             <h3>{post.title}</h3>
             <p>{post.body}</p>
