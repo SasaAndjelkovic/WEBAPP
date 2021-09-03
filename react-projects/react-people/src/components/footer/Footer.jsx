d2363ce8619d6de007b205deddcd217e108656e9
SasaAndjelkovic <sandjelkovickg@gmail.com>
2021-09-03 18:32:51 +0200

    footer

---
+++ react-projects/react-people/src/components/footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+    return (
+        <footer>
+            <p> © 2021 Sasa & Friends</p>
+            <p className ='counterDiv'></p>
+        </footer>
+    );
+}
+
+export default Footer;
