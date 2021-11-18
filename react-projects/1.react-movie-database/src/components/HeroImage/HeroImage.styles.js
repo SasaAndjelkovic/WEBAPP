e648397f5d362a80209bafd0a5395a6ecfc78e03
SasaAndjelkovic <sandjelkovickg@gmail.com>
2021-11-18 17:31:04 +0100

    Grid component

--- react-projects/1.react-movie-database/src/components/HeroImage/HeroImage.styles.js
+++ react-projects/1.react-movie-database/src/components/HeroImage/HeroImage.styles.js
@@ -3,8 +3,8 @@ import styled from 'styled-components';
 export const Wrapper = styled.div`
     background: linear-gradient(
         to bottom, 
-        rgba (0, 0, 0, 0) 41%,
-        rgba (0, 0, 0, 0.65) 100%
+        rgba(0, 0, 0, 0) 41%,
+        rgba(0, 0, 0, 0.65) 100%
     ),
     url(${({ image }) => image}), var(--darkGrey);
     background-size: 100%, cover;
@@ -55,6 +55,6 @@ export const Text = styled.div`
     }
 
     @media screen and (max-width: 720px) {
-        font-size: var(--fontSmall);
+        max-width: 100%;
     }
 `;
--- react-projects/1.react-movie-database/src/components/Home.js
+++ react-projects/1.react-movie-database/src/components/Home.js
@@ -5,6 +5,7 @@ import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from '../config';
 
 //Components
 import HeroImage from './HeroImage';
+import Grid from './Grid/Index';
 
 //Hook
 import { useHomeFetch } from '../hooks/useHomeFetch';
@@ -19,17 +20,21 @@ const Home = () => {
 
      //div
     return (
-    <>  
-        {state.results[0] ?
-        <HeroImage 
-            image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`}
-            title={state.results[0].original_title}
-            text={state.results[0].overview}
-        />
-        : null   
-        }
-    </>
-    )
-}
+        <>  
+            {state.results[0] ? ( 
+                <HeroImage 
+                    image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`}
+                    title={state.results[0].original_title}
+                    text={state.results[0].overview}
+                />
+            ) : null }
+            <Grid header='Popular Movies'>
+                {state.results.map(movie => (
+                    <div key={movie.id}>{movie.title}</div>
+                ))}
+            </Grid>
+        </>
+    );
+};
 
 export default Home;
