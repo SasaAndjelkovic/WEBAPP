e5e9360ba9ec51fbc6cae9ca0d0f93aaee9557d7
SasaAndjelkovic <sandjelkovickg@gmail.com>
2021-11-23 14:44:15 +0100

    Routing with React Router

--- react-projects/1.react-movie-database/src/App.js
+++ react-projects/1.react-movie-database/src/App.js
@@ -1,7 +1,13 @@
 import React from 'react';
+
+//Routing
+import { BrowseRouter as Router, Routes, Route } from 'react-router-dom';
+
 //Components
 import Header from './components/Header';
 import Home from './components/Home';
+import Movie from './components/Movie';
+import NotFound from './components/NotFound';
 
 //styles
 import { GlobalStyle } from './GlobalStyle';
@@ -9,16 +15,26 @@ import { GlobalStyle } from './GlobalStyle';
 //primer da ne mora da se koristi JSX
 //const Star = () => React.createElement ('div, null, 'This is a little star' ) 
 // null, zato sto jos uvek nema nijedan props
-
-const App = () => {
+/*const App = () => (  
   //return Star()
-  return (
     <div className="App">
       <Header />
       <Home />
       <GlobalStyle />
     </div>
+  );*/
+
+const App = () => (  
+    <Router> 
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/:moveId' element={<Movie />} />
+        <Route path='/*' element={<NotFound />} />
+      </Routes>
+      <GlobalStyle />
+    </Router>
   );
-}
+
 
 export default App;
