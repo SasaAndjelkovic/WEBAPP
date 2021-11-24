a4675d0eab65c607a60f7c23c7b8b4da3e4fd5c9
SasaAndjelkovic <sandjelkovickg@gmail.com>
2021-11-24 19:22:28 +0100

    Fetch movie data from the API

--- react-projects/1.react-movie-database/src/components/Movie.js
+++ react-projects/1.react-movie-database/src/components/Movie.js
@@ -1,5 +1,32 @@
 import React from 'react';
+import { useParams } from 'react-router';
 
-const Movie = () => <div>Movie</div>;
+//Config
+import { IMAGE_BASE_URL, POSTER_SIZE } from '../config';
+
+//Components
+import Grid from './Grid/Index';
+import Spinner from './Spinner/Index';
+
+//Hook
+import { useMovieFetch } from '../hooks/useMovieFetch';
+
+//Image
+import NoImage from '../images/no_image.jpg';
+
+//const Movie = () => <div>Movie</div>;
+const Movie = () => {
+    const { movieId } = useParams();
+
+    const { state: movie, loading, error } = useMovieFetch(movieId);
+
+    console.log(movie);
+    
+    return (
+        <>
+            <div>Movie</div>
+        </>
+    );
+};
 
 export default Movie;
