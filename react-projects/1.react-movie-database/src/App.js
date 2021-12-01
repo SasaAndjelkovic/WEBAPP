e9e9f04da0677265041203c87a07c2d643e5f0f8
SasaAndjelkovic <sandjelkovickg@gmail.com>
2021-12-01 10:47:04 +0100

    Login

--- react-projects/1.react-movie-database/src/App.js
+++ react-projects/1.react-movie-database/src/App.js
@@ -8,9 +8,12 @@ import Header from './components/Header';
 import Home from './components/Home';
 import Movie from './components/Movie';
 import NotFound from './components/NotFound';
+import Login from './components/Login';
+
+//Context
+import UserProvider from './context';
 
 //styles
-
 
 import { GlobalStyle } from './GlobalStyle';
 
@@ -28,15 +31,17 @@ import { GlobalStyle } from './GlobalStyle';
 
 const App = () => (  
     <Router> 
-      <Header />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/:moveId' element={<Movie />} />
-        <Route path='/*' element={<NotFound />} />
-      </Routes>
-      <GlobalStyle />
+      <UserProvider>
+        <Header />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/:moveId' element={<Movie />} />
+          <Route path='/*' element={<NotFound />} />
+        </Routes>
+        <GlobalStyle />
+      </UserProvider>
     </Router>
   );
-
 
 export default App;
