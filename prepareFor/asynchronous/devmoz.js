47697c7a3e9ddb9261711ac3431c72316aaa6c39
SasaAndjelkovic <sandjelkovickg@gmail.com>
2021-12-15 00:12:18 +0100

    new commit

--- prepareFor/asynchronous/devmoz.js
+++ prepareFor/asynchronous/devmoz.js
@@ -50,5 +50,19 @@ myFetch()
     console.log('There has been a problem with your fetch operation:' + e.message)
 });
 
-//7. 
+//7. druga polovina funkcije u novi blok, da bi je ucinili fleksibilnijom
+const myFetch = async() => {
+    let response = await fetch('coffee.jpg');
+    if (!response.ok) {
+        throw new Error(`HTTP error!status:${response.status}`);
+    }
+    return await response.blob();
+}
+
+myFetch().then((blob) => {   
+    let objectURL = URL.createObjectURL(blob); //Blob
+    let image = document.createElement('img');
+    image.src = objectURL;
+    document.body.appendChild(image);
+}).catch(e=>console.log(e));
 
